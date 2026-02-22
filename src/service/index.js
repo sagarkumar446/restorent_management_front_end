614b25ebffea65bcd3148a2318213394c0da034d
sagar kumar <sagarkumar020599@gmail.com>
2026-02-22 09:01:00 +0530

    fix: Update baseURL to fallback to localhost for local development

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api'
+export const baseURL = import.meta.env.VITE_API_BASE_URL 
 
 export default (headers = {}) => {
     const service = axios.create({
